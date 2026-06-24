270cd32b7022b0c57b5ba30f05f8e5b26ea479a4
braxtonlemmon <braxtonlemmon@gmail.com>
2026-06-24 14:54:57 -0600

    add import sort dep and eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,23 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "no-restricted-syntax": [
+        "error",
+        {
+          selector: "ImportDeclaration[source.value=/^\\.\\.\\/|^\\.\\.\\\\/]",
+          message:
+            "Use absolute imports with @ symbol instead of relative parent imports",
+        },
+      ],
+    },
+  },
 ]);
 
 export default eslintConfig;
