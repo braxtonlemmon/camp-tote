3602767ac660267305b209238440519bde528203
braxtonlemmon <braxtonlemmon@gmail.com>
2026-06-24 15:48:09 -0600

    initial project dependencies and package.json updates

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
