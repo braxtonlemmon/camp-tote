03887c2c4a553e69d99f1ab5a38da02f7fcaeafe
braxtonlemmon <braxtonlemmon@gmail.com>
2026-06-24 15:50:39 -0600

    chore: Initial dependencies (#1)
    * add import sort dep and eslint rules
    
    * update package.json
    
    * initial project dependencies and package.json updates

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +14,23 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "no-restricted-syntax": [
+        "error",
+        {
+          selector: "ImportDeclaration[source.value=/^\\.\\.\\/|^\\.\\.\\\\/]",
+          message:
+            "Use absolute imports with @ symbol instead of relative parent imports",
+        },
+      ],
+    },
+  },
 ]);
 
 export default eslintConfig;
